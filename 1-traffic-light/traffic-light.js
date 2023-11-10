acce67e0774cba2b8a27b11badac05591399c0da
Ali Othman <alioth840@gmail.com>
2023-11-10 18:19:48 +0100

    js-week2-prep-exercises

---
+++ 1-traffic-light/traffic-light.js
@@ -0,0 +1,52 @@
+"use strict";
+/**
+ * The `trafficLight` object is now no longer a global variable. Instead,
+ * it is defined in function `main()` and passed as a parameter to other
+ * functions, as and when needed.
+ */
+
+function getCurrentState(trafficLight) {
+  return trafficLight.possibleStates[trafficLight.stateIndex];
+}
+
+function getNextStateIndex(trafficLight) {
+  
+  trafficLight.stateIndex = (trafficLight.stateIndex + 1) % trafficLight.possibleStates.length;
+
+  return trafficLight.stateIndex;
+}
+
+function waitSync(secs) {
+  const start = Date.now();
+  while (Date.now() - start < secs * 1000) {
+  }
+}
+
+function main() {
+  const trafficLight = {
+    possibleStates: ["green", "orange", "red"],
+    stateIndex: 0,
+  };
+
+  for (let cycle = 0; cycle < 6; cycle++) {
+    const currentState = getCurrentState(trafficLight);
+    console.log(cycle, "The traffic light is now", currentState);
+
+    waitSync(1);
+    getNextStateIndex(trafficLight);
+  }
+}
+
+main();
+
+/**
+ * The output should be:
+
+0 The traffic light is now green
+1 The traffic light is now orange
+2 The traffic light is now red
+3 The traffic light is now green
+4 The traffic light is now orange
+5 The traffic light is now red
+
+*/
